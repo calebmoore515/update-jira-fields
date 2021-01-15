8854f19b8e07697c890208dca24f52ed189ed56d
calebmoore515 <50252545+calebmoore515@users.noreply.github.com>
2021-01-14 18:40:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ options = {'server': 'https://jira.server.com'}
 jira = JIRA(options, basic_auth=('username', 'pass'))
 
 # JQL Query
-issues_in_proj = jira.search_issues('project = CAM AND issuetype = "Targeting Strategy" '
-                                    'AND status = Fulfillment AND "Fulfill Channel ID" ~ "788" AND labels is EMPTY',
+issues_in_proj = jira.search_issues('project = ABC AND issuetype = "Ticket Type" '
+                                    'AND status = Open AND "Fulfill Channel ID" ~ "123" AND labels is EMPTY',
                                     maxResults=50)
 
 # List to append regex-ed ids into
@@ -27,11 +27,11 @@ print('Pin Tickets Updated: \n')
 # Loop that pulls original id map, subs in (788) using regex and then updates the field
 for issue in issues_in_proj:
     print('Strategy Ticket: https://jira.server.com/browse/' + str(issue))
-    print('Original Mapping: ' + issue.fields.customfield_11502)
-    mapping =(re.sub(r'\((\d+)*\)', "(788)", issue.fields.customfield_11502))
+    print('Original Mapping: ' + issue.fields.customfield_12345)
+    mapping =(re.sub(r'\((\d+)*\)', "(788)", issue.fields.customfield_12345))
     legacy_id.append(mapping)
-    issue.update(fields={'customfield_11502': (str(legacy_id)[2:-2])})
-    print('Updated Mapping: ' + (str(legacy_id)[2:-2]))  # The [2:-2] may break the logic
+    issue.update(fields={'customfield_12345': (str(legacy_id)[2:-2])})
+    print('Updated Mapping: ' + (str(legacy_id)[2:-2]))
     legacy_id.clear()
     issue.update(fields={"labels": ['av.mapped']})
     print('Added av.mapped label to ticket\n')
